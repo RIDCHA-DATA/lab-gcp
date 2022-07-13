47dfe749e64739e6a3510f61a8cbd4af0632e55a
Ahmed Belhoula <ahmd.belhoula@gmail.com>
2022-07-14 00:43:38 +0200

    fix sa & workloadidentity for github ci

--- modules/WORKLOAD-IDENTIY/main.tf
+++ modules/WORKLOAD-IDENTIY/main.tf
@@ -3,19 +3,26 @@ module "service_accounts" {
   project_id    = var.project_id
   prefix        = var.prefix
   names         = var.account_names
-  project_roles = ["${var.project_id}=>roles/viewer"]
+  project_roles = [
+    "${var.project_id}=>roles/viewer",
+    "${var.project_id}=>roles/storage.objectViewer",
+    "${var.project_id}=>roles/container.clusterAdmin",
+    "${var.project_id}=>roles/iam.workloadIdentityUser",
+  ]
   display_name  = "gh Account"
   description   = "GH Account for CI purpose"
 }
 
+
 module "gh_oidc" {
+  for_each = toset(var.account_names)
   source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
   project_id  = var.project_id
   pool_id     = var.wl_pool_name
   provider_id = var.wl_provider_id
   sa_mapping = {
-    "var.account_names" = {
-      sa_name   = "projects/my-project/serviceAccounts/foo-service-account@my-project.iam.gserviceaccount.com"
+    "each.value" = {
+      sa_name   = "projects/${var.project_id}/serviceAccounts/${each.value}@${var.project_id}.iam.gserviceaccount.com"
       attribute = "attribute.repository/user/repo"
     }
   }
--- modules/WORKLOAD-IDENTIY/terraform.tfvars
+++ modules/WORKLOAD-IDENTIY/terraform.tfvars
@@ -2,6 +2,7 @@ project_id = "vivid-rush-227416"
 region     = "asia-east1"
 zone       = "asia-east1-b"
 
-wl_pool_name = "github-pool"
-wl_provider_id = "github-provider"
-account_names = [ "gh sa" ]
+wl_pool_name = "gh-pool"
+wl_provider_id = "gh-provider"
+account_names = [ "gh-account" ]
+project_roles = [ "value" ]
