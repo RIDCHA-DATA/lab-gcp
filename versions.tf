0dc701e1f2407dffb67de76c3bf782a8fd25da34
Ahmed Belhoula <ahmd.belhoula@gmail.com>
2022-07-15 15:17:51 +0200

    add gcs as default backend for tfstate
    Signed-off-by: Ahmed Belhoula <ahmd.belhoula@gmail.com>

--- versions.tf
+++ versions.tf
@@ -1,4 +1,8 @@
 terraform {
+  backend "gcs" {
+    bucket = "dataprodev-01"
+    prefix = "terraform/state/terraform.tfstate"
+  }
   required_providers {
     google = {
       source = "hashicorp/google"
